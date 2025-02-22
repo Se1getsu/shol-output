4b059bc363e1a810be169880abed43e83f4a6540
ktohda <ktohda0127@gmail.com>
2025-02-22 23:49:00 +0900

    nGenつくったよ

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, hash::Hash};
 
 #[derive(Eq, Hash, Debug, PartialEq)]
 enum ResourceType {
@@ -6,15 +6,18 @@ enum ResourceType {
     Int(i32)
 }
 
-struct _nGen {
+struct _kemo {
     resources: HashSet<ResourceType>
 }
 
-impl _nGen {
+impl _kemo {
     fn rule(&mut self) {
         let mut to_replace = Vec::new();
         
-        // 変更が必要な要素を特定
+        /*
+        . $ < 4 # $ + 1 
+        | $ % 2 = 0 # $ * 10
+        */
         for resource in self.resources.iter() {
             if let ResourceType::Int(value) = resource {
                 if *value < 4 {
@@ -34,18 +37,57 @@ impl _nGen {
     }
 }
 
+struct _nGen {
+    resources: HashSet<ResourceType>
+}
+
+struct _fizzbuzz {
+    resources: HashSet<ResourceType>
+}
+
+impl _nGen {
+    fn rule(&mut self, _fizzbuzz_resources: &mut HashSet<ResourceType>) {
+        let mut to_replace = Vec::new();
+        
+        /*
+        . $ < 100 # $ + 1 
+        | $ #fizzbuzz $
+        */
+        for resource in self.resources.iter() {
+            if let ResourceType::Int(value) = resource {
+                if *value < 100 {
+                    to_replace.push((ResourceType::Int(*value), ResourceType::Int(*value + 1)));
+                }
+                _fizzbuzz_resources.insert(ResourceType::Int(*value));
+            }
+        }
+        
+        // 要素の置き換え
+        for (old, new) in to_replace {
+            self.resources.remove(&old);
+            self.resources.insert(new);
+        }        
+    }
+}
+
+impl _fizzbuzz {
+    fn rule(&mut self) {
+        println!("{:?}", self.resources);
+    }
+}
+
 fn main() {
     let mut nGen = _nGen {
-        resources: HashSet::new(),
+        resources: HashSet::new()
+    };
+    let mut fizzbuzz = _fizzbuzz {
+        resources: HashSet::new()
     };
     nGen.resources.insert(ResourceType::Int(1));
-    nGen.resources.insert(ResourceType::String("Hello".to_string()));
-    nGen.rule();
-    println!("{:?}", nGen.resources);
-    nGen.rule();
-    println!("{:?}", nGen.resources);
-    nGen.rule();
-    println!("{:?}", nGen.resources);
-    nGen.rule();
-    println!("{:?}", nGen.resources);
+    nGen.rule(&mut fizzbuzz.resources);
+    fizzbuzz.rule();
+    nGen.rule(&mut fizzbuzz.resources);
+    fizzbuzz.rule();
+    nGen.rule(&mut fizzbuzz.resources);
+    fizzbuzz.rule();
 }
